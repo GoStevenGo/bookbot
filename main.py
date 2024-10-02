0ceaae51e9bc7fe8b7f8cf8d20c8e35e6a5b0065
GoStevenGo <stevenpaulgomez@gmail.com>
2024-10-02 05:53:00 -0500

    updated main.py to provide a count per character in text

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     count_words(file_contents)
+    count_characters(file_contents)
 
-def count_words(file_contents):
-    #using .split() to split a string on whitespace
-    words = file_contents.split()
-    
+def count_words(file_contents): #takes in a str
+    words = file_contents.split() #using .split() to split a string on whitespace into a list
     count = 0
     for word in words:
         count += 1
-    
     print(count)
+
+def count_characters(file_contents): #takes in a str
+    characters = {} #Using a dictionary where String -> Integer (Key -> Value, aka Key:Value pair)
+    lowered_string = file_contents.lower() #Using .lower() to convert str to lowercase. Easier to parce through for dictionary
+    for i in lowered_string: #loop through the string character by character
+        if i not in characters:
+            characters[i] = 1
+        else:
+            characters[i] += 1
+    print(characters)
 
 main()
